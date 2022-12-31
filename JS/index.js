ff3c9f67068d9265f3c8ccef56da8af76f0fa1c9
Omari99 <mariam.ali01@student.moringaschool.com>
2022-12-31 17:52:37 +0300

    posting data

--- JS/index.js
+++ JS/index.js
@@ -13,36 +13,65 @@ fetch("https://miriam-db-production.up.railway.app/menus")
 
 })
 
-
-
-
-const API = ("https://miriam-db-production.up.railway.app/menus")
 document.addEventListener('DOMContentLoaded', () =>{
 
 // enroll
     const enroll = document.querySelector('#enroll');
-    enroll.addEventListener('click', clickAlert);
+    enroll.addEventListener('click', clickAlert1);
     
-    function clickAlert() {
+    function clickAlert1() {
       alert('Join us now!');
     }
+    // Subscribe
     const signUp = document.querySelector('#btn');
+    console.log(signUp)
     signUp.addEventListener('click', clickAlert);
     
     function clickAlert() {
-      alert('We are glad that you signed up for our menus!');
+      alert('you subscribed for our menus!');
     }
 
     function emailSubmit(event){
       console.log(`Email submitted Time Stamp: ${event.timeStamp}`) ;
       event.preventDefault();
     }
-    const email=
-    document.getElementById("btn");
-    email.addEventListener(`click`, emailSubmit)  
+   signUp.addEventListener(`click`, emailSubmit)  
      
     })
-    
+    const form = document.getElementById('form')
+    form.addEventListener('submit', function(event){
+      event.preventDefault();
+      
+      const username= document.getElementById('username').value 
+      // console.log(username);
+
+      const email= document.getElementById('email').value 
+      // console.log(email);
+;
+      const  phone= document.getElementById('phone').value 
+      // console.log(phone);
+
+      const data ={ 
+        name: username,
+        email: email,
+        phone : phone
+        
+      }
+
+fetch ("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+  },
+  body: JSON.stringify(data),
+}).then(response => {
+  return response.json()
+}).then(data=> {
+  console.log(data)
+  
+})
+    })
     
 
 
